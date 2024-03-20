e3471de62a782f1c511863ab78001bc1d1d9895d
Haggay <info@simplyhost.co.il>
2024-03-20 12:35:15 +0200

    Enable share video file Enable share video file

--- app/src/main/java/com/sensoguard/hunter/services/MediaService.kt
+++ app/src/main/java/com/sensoguard/hunter/services/MediaService.kt
@@ -1,4 +1,4 @@
-//package com.sensoguard.hunter.services
+package com.sensoguard.hunter.services//package com.sensoguard.hunter.services
 //
 //import android.app.NotificationChannel
 //import android.app.NotificationManager
